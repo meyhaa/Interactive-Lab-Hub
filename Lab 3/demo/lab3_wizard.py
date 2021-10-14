23baed4e37662a9bd7a9bcbd3c83e21720ab53ed
Meyhaa Buvanesh <meyhaa@gmail.com>
2021-10-13 21:13:15 -0400

    Add lab3 wizard version of demo

---
+++ Lab 3/demo/lab3_wizard.py
@@ -0,0 +1,96 @@
+import eventlet
+eventlet.monkey_patch()
+
+from flask import Flask, Response,render_template
+from flask_socketio import SocketIO, send, emit
+from subprocess import Popen, call
+
+import time
+import board
+import busio
+import adafruit_mpu6050
+import json
+import socket
+
+import signal
+import sys
+from queue import Queue
+
+from __future__ import print_function
+import qwiic_joystick
+import time
+import sys
+
+ 
+i2c = busio.I2C(board.SCL, board.SDA)
+mpu = adafruit_mpu6050.MPU6050(i2c)
+
+hostname = socket.gethostname()
+hardware = 'plughw:2,0'
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+audio_stream = Popen("/usr/bin/cvlc alsa://"+hardware+" --sout='#transcode{vcodec=none,acodec=mp3,ab=256,channels=2,samplerate=44100,scodec=none}:http{mux=mp3,dst=:8080/}' --no-sout-all --sout-keep", shell=True)
+
+@socketio.on('speak')
+def handel_speak(val):
+    call(f"espeak '{val}'", shell=True)
+
+@socketio.on('connect')
+def test_connect():
+    print('connected')
+    emit('after connect',  {'data':'Lets dance'})
+
+@socketio.on('ping-gps')
+def handle_message(val):
+    # print(mpu.acceleration)
+    emit('pong-gps', mpu.acceleration) 
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', hostname=hostname)
+
+def signal_handler(sig, frame):
+    print('Closing Gracefully')
+    audio_stream.terminate()
+    sys.exit(0)
+
+def runExample():
+
+    print("\nSparkFun qwiic Joystick   Example 1\n")
+    myJoystick = qwiic_joystick.QwiicJoystick()
+
+    if myJoystick.isConnected() == False:
+        print("The Qwiic Joystick device isn't connected to the system. Please check your connection", \
+            file=sys.stderr)
+        return
+
+    myJoystick.begin()
+
+    print("Initialized. Firmware Version: %s" % myJoystick.getVersion())
+
+    while True:
+
+        print("X: %d, Y: %d, Button: %d" % ( \
+                    myJoystick.getHorizontal(), \
+                    myJoystick.getVertical(), \
+                    myJoystick.getButton()))
+
+        time.sleep(.5)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+
+if __name__ == "__main__":
+    socketio.run(app, host='0.0.0.0', port=5000)
+
+    try:
+        runExample()
+    except (KeyboardInterrupt, SystemExit) as exErr:
+        print("\nEnding Example 1")
+        sys.exit(0)
+
+
